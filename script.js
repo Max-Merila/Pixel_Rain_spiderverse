06e743ff7ceb153a76fc83e92d76938d7622bbb9
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2021-12-15 12:59:17 -0600

    Add files via upload
    finished the video, now spiderman is transparent and has pixels defining him by the brightness of red green and blue

--- script.js
+++ script.js
@@ -8,9 +8,36 @@ myImage.addEventListener('load', function(){
   canvas.height = 630;
 
   ctx.drawImage(myImage, 0 , 0, canvas.width, canvas.height);
+  const pixels = ctx.getImageData( 0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   let particlesArray = [];
   const numberOfParticles = 5000;
+
+  let mappedImage = [];
+  for (let y = 0; y < canvas.height; y++){
+    let row = [];
+    for (let x=0; x < canvas.width; x++){
+      const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+      const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+      const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+      const brightness = calculateRelativeBrightness(red, green, blue);
+      const cell = [
+        cellBrightness = brightness,
+      ];
+      row.push(cell);
+    }
+    mappedImage.push(row)
+  }
+console.log(mappedImage)
+
+  function calculateRelativeBrightness(red, green, blue){
+    return Math.sqrt(
+      (red * red) * 0.299 +
+      (green * green) * 0.57 +
+      (blue * blue) * 0.114
+    )/100;
+  }
 
   class Particle {
     constructor(){
@@ -19,9 +46,15 @@ myImage.addEventListener('load', function(){
       this.speed = 0;
       this.velocity = Math.random() * 0.5;
       this.size = Math.random() * 1.5 + 1;
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
     }
     update(){
-      this.y+= this.velocity;
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+      this.speed = mappedImage[this.position1][this.position2][0];
+      let movement = (2.5 - this.speed) + this.velocity;
+      this.y+= movement;
       if (this.y >= canvas.height){
         this.y = 0;
         this.x = Math.random() * canvas.width;
@@ -41,11 +74,14 @@ myImage.addEventListener('load', function(){
   }
   init();
   function animate(){
+    //ctx.drawImage(myImage, 0 , 0, canvas.width, canvas.height);
     ctx.globalAlpha = 0.05;
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.02;
     for(let i = 0; i < particlesArray.length; i++){
       particlesArray[i].update();
+      ctx.globalAlpha = particlesArray[i].speed * 0.5;
       particlesArray[i].draw();
     }
     requestAnimationFrame(animate);
